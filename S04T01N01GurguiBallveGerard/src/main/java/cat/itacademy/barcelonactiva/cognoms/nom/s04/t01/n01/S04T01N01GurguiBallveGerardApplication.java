70aa139f3366eea9ec1fe901fa150bf958f61132
GerardGurgui <105665705+GerardGurgui@users.noreply.github.com>
2022-09-13 09:25:10 +0200

    Add files via upload

---
+++ S04T01N01GurguiBallveGerard/src/main/java/cat/itacademy/barcelonactiva/cognoms/nom/s04/t01/n01/S04T01N01GurguiBallveGerardApplication.java
@@ -0,0 +1,17 @@
+package cat.itacademy.barcelonactiva.cognoms.nom.s04.t01.n01;
+
+import cat.itacademy.barcelonactiva.cognoms.nom.s04.t01.n01.controllers.HelloWorldController;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class S04T01N01GurguiBallveGerardApplication {
+
+	public static void main(String[] args) {
+
+		ApplicationContext context = SpringApplication.run(S04T01N01GurguiBallveGerardApplication.class, args);
+
+	}
+
+}
